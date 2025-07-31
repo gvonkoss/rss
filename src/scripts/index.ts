94695a8cc870a5be6a84c19bf515690ef11b4542
Gabrielle von Koss <gabrielle@gvonkoss.com>
2025-07-31 19:10:23 +0200

    update styling and some podcast logic

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,15 +1,7 @@
 import Parser from 'rss-parser';
+import { sources } from './podcasts';
 
 const parser = new Parser();
-
-const sources = [
-  'https://feeds.simplecast.com/p7S4nr_h', // The Anthropocene Reviewed
-  'https://feeds.megaphone.fm/VMP8871377602', // This is Love
-  'https://feeds.megaphone.fm/LIT1443896445', // Emergence Magazine
-  // 'https://feeds.simplecast.com/AuAxH_Bf', // On Being
-  'https://feeds.simplecast.com/FO6kxYGj', // Ologies
-  'https://rss.amperwave.net/v2/feed/podcorn/4b34b711a4a1ede47c37820155127439', // Deadtalks
-];
 
 interface FeedItem {
   feed?: string;
---
+++ src/scripts/podcasts.ts
@@ -0,0 +1,14 @@
+export const PODCASTS = {
+  'The Anthropocene Reviewed': 'https://feeds.simplecast.com/p7S4nr_h',
+  'This is Love': 'https://feeds.megaphone.fm/VMP8871377602',
+  'Emergence Magazine Podcast': 'https://feeds.megaphone.fm/LIT1443896445',
+  'Ologies with Alie Ward': 'https://feeds.simplecast.com/FO6kxYGj',
+  'Dead Talks':
+    'https://rss.amperwave.net/v2/feed/podcorn/4b34b711a4a1ede47c37820155127439',
+  'My Dad Wrote A Porno':
+    'https://feeds.acast.com/public/shows/mydadwroteaporno',
+  '99% Invisible': 'https://feeds.simplecast.com/BqbsxVfO',
+};
+
+export const titles = Object.keys(PODCASTS);
+export const sources = Object.values(PODCASTS);
